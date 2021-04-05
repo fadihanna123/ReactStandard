1bcfd8c7edd6c34be35dd6b6691fbe825341204c
Fadi <fadi@Fadis-MacBook-Pro.local>
2021-04-05 21:19:17 +0200

    Upgrade packages och make url shorter

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-
-import App from "./App";
+import App from 'App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
