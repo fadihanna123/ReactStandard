2561724dc531ea251977b56c95facd83f7d582e1
fadihanna123 <fhanna181@gmail.com>
2021-05-11 22:28:20 +0200

    Upgrade packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
+import "sass/main.scss";
+
 import App from "App";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import "sass/main.scss";
 
 ReactDOM.render(
   <React.StrictMode>
